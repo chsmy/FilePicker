2ae80c94c0fac625764da03df4b7d5e030c33810
caoweiaaa <13161083183@163.com>
2017-08-25 18:50:49 +0800

    更改查询条件

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # FilePicker
 文件选择器
+
+包括常用文件和全部文件
+
+>常用文件：
+通过媒体查询MediaStore.Files
+demo中查询的是微信和QQ中的图片和下载的文件
+
+>全部文件：
+ 通过根路径找到相应的files展示
+ 
+效果图：
+![](https://github.com/chsmy/FilePicker/blob/master/images/2012.gif)
+
--- app/src/main/java/com/chs/filepicker/filepicker/util/FileUtils.java
+++ app/src/main/java/com/chs/filepicker/filepicker/util/FileUtils.java
@@ -43,33 +43,11 @@ public class FileUtils {
         return entities;
     }
 
-    public static String cutLastSegmentOfPath(String path) {
-        return path.substring(0, path.lastIndexOf("/"));
-    }
 
     public static String getReadableFileSize(long size) {
         if (size <= 0) return "0";
         final String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
         int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
         return new DecimalFormat("#.##").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
-    }
-
-    public static String convertFileSize(long size) {
-        long kb = 1024;
-        long mb = kb * 1024;
-        long gb = mb * 1024;
-        //%.2f 即是保留两位小数的浮点数，后面跟上对应单位就可以了，不得不说java很方便
-        if (size >= gb) {
-            return String.format("%.2f GB", (float) size / gb);
-        } else if (size >= mb) {
-            float f = (float) size / mb;
-            //如果大于100MB就不用保留小数位啦
-            return String.format(f > 100 ? "%.0f MB" : "%.2f MB", f);
-        } else if (size >= kb) {
-            float f = (float) size / kb;
-            //如果大于100kB就不用保留小数位了
-            return String.format(f > 100 ? "%.0f KB" : "%.2f KB", f);
-        } else
-            return String.format("%d B", size);
     }
 }
